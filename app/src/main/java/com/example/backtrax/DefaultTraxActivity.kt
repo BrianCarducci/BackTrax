b8f38ace48ee1cd111390727aa91cd1eec9075f9
BrianCarducci <briancarducci@gmail.com>
2020-01-17 14:17:40 -0500

    Added a listview and organized player buttons

--- app/src/main/java/com/example/backtrax/DefaultTraxActivity.kt
+++ app/src/main/java/com/example/backtrax/DefaultTraxActivity.kt
@@ -2,11 +2,22 @@ package com.example.backtrax
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class DefaultTraxActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_default_trax)
+
+        val testArray = arrayOf("hi", "bye","hi", "bye","hi", "bye","hi", "bye","hi", "bye","hi", "bye","hi", "bye","hi", "bye")
+
+        val arrayAdapter = ArrayAdapter<String>(this, R.layout.song_in_listview, testArray)
+        findViewById<ListView>(R.id.songs_list_view).apply {
+            adapter = arrayAdapter
+        }
     }
+
+
 }
