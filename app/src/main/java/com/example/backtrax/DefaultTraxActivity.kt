589b173968ffa5fdfb992ed74fe2b9dbae98d2ca
BrianCarducci <briancarducci@gmail.com>
2020-01-19 21:39:30 -0500

    can play sounds

--- app/src/main/java/com/example/backtrax/DefaultTraxActivity.kt
+++ app/src/main/java/com/example/backtrax/DefaultTraxActivity.kt
@@ -1,9 +1,16 @@
 package com.example.backtrax
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_default_trax.*
+
+
+var mediaPlayer: MediaPlayer? = null
 
 class DefaultTraxActivity : AppCompatActivity() {
 
@@ -11,13 +18,28 @@ class DefaultTraxActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_default_trax)
 
-        val testArray = arrayOf("hi", "bye","hi", "bye","hi", "bye","hi", "bye","hi", "bye","hi", "bye","hi", "bye","hi", "bye")
-
-        val arrayAdapter = ArrayAdapter<String>(this, R.layout.song_in_listview, testArray)
-        findViewById<ListView>(R.id.songs_list_view).apply {
+        val arrayAdapter = ArrayAdapter<String>(this, R.layout.song_in_listview, assets.list("mp3s"))
+        songsListView.apply {
             adapter = arrayAdapter
         }
+        songsListView.setOnItemClickListener { parent, view, position, id ->
+            val element = arrayAdapter.getItem(position)
+            val afd = assets.openFd("mp3s/yournightmp3.mp3")
+            mediaPlayer = MediaPlayer.create(applicationContext, R.raw.yournightmp3)
+            mediaPlayer?.start()
+//            mediaPlayer?.reset()
+//            mediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+//            mediaPlayer?.prepare()
+//            mediaPlayer?.start()
+            Log.d("clicked file", afd.toString())
+
+        }
     }
+
+    fun stop(view: View) {
+        mediaPlayer?.reset()
+    }
+
 
 
 }
